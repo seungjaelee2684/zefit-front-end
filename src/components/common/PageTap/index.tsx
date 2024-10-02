0020f2f6b6aa6f8ea34e0829232ad4bb625729a1
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-10-02 16:48:19 +0900

    <Feat: 회사 개요 페이지 마크업 작업>

--- src/components/common/PageTap/index.tsx
+++ src/components/common/PageTap/index.tsx
@@ -1,7 +1,39 @@
+import { aboutNavList } from '@/data/navData';
+import './style.css';
+import { usePathname } from 'next/navigation';
+
 export default function PageTap() {
+
+    const path = usePathname();
+    
     return (
         <section className='page_tap_container'>
-            
+            <nav className='page_tap_out_wrapper'>
+                <a href='/' className='home_button_tap'>
+                   <img
+                        src='/icons/home.png'
+                        alt='홈 아이콘'
+                        className='home_icon' /> 
+                </a>
+                <ul className='page_tap_in_wrapper'>
+                    {aboutNavList?.map((item: any, index: number) =>
+                        <li key={index}>
+                            <a
+                                href={(path !== item.href) && item.href}
+                                style={{
+                                    cursor: (path === item.href) ? 'default' : 'pointer'
+                                }}
+                                className={
+                                    (path === item.href)
+                                        ? 'page_tap_click_button'
+                                        : 'page_tap_button'
+                                    }>
+                                {item.id}
+                            </a>
+                        </li>
+                    )}
+                </ul>
+            </nav>
         </section>
     )
 };
---
+++ src/data/companyData.tsx
@@ -0,0 +1,29 @@
+export const companyData = [
+    {
+        id: '모델',
+        icon: 'http://www.zefit.co.kr/theme/basic/assets/images/zefit/i-img2.png',
+        title: '맞춤형 제브라피쉬\n질병모델',
+        content: '보유한 유전자 편집기술을 활용하여 질환 맞춤형 질병모델을 제작할 수 있는 전문인력과 실험동물을 사육할 수 있는 시설을 확보하고 있습니다.',
+        color: '#00AEEF',
+        titlecolor: '#',
+        textcolor: '#E7F8FF'
+    },
+    {
+        id: '인프라',
+        icon: 'http://www.zefit.co.kr/theme/basic/assets/images/zefit/i-img3.png',
+        title: '자동화된 실험\n인프라',
+        content: '자동화된 실험 장비는 신속한 실험이 가능하도록 하며 편차가 적은 데이터를 확보할 수 있게 합니다.',
+        color: '#0190D6',
+        titlecolor: '#004262',
+        textcolor: '#EEF9FF'
+    },
+    {
+        id: '빅데이터',
+        icon: 'http://www.zefit.co.kr/theme/basic/assets/images/zefit/i-img4.png',
+        title: 'In-vivo 대상 약리\n효과 빅데이터',
+        content: 'In-vivo 단계의 화합물 테스트 빅데이터를 확보하고 있으며 이를 기반으로 화합물의 안전성과 유효성을 예측합니다.',
+        color: '#0055A7',
+        titlecolor: '#00274C',
+        textcolor: '#EEF9FF'
+    },
+];
